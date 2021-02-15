1f5422d22424897b848d7b7743dee9d2149bc4eb
Gabriel Ramirez <gabriel.rmrz.snchz@gmail.com>
2021-02-15 14:51:53 +0100

    Adding multiprocess rootuplizer

---
+++ scripts/makeInputFilesList.sh
@@ -0,0 +1,14 @@
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/Charmonium/crab_data_Run2018A_UL/210212_112353/0000/RJPsi_data_2021Feb12_%s.root\n" {1..999}
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/Charmonium/crab_data_Run2018A_UL/210212_112353/0001/RJPsi_data_2021Feb12_%s.root\n" {1000..1999}
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/Charmonium/crab_data_Run2018A_UL/210212_112353/0002/RJPsi_data_2021Feb12_%s.root\n" {2000..2923}
+
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/DoubleMuonLowMass/crab_data_DoubleMuLowMass_Run2018A_UL/210212_112725/0000/RJPsi_data_2021Feb12_%s.root\n" {1..999}
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/DoubleMuonLowMass/crab_data_DoubleMuLowMass_Run2018A_UL/210212_112725/0001/RJPsi_data_2021Feb12_%s.root\n" {1000..1999}
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/DoubleMuonLowMass/crab_data_DoubleMuLowMass_Run2018A_UL/210212_112725/0002/RJPsi_data_2021Feb12_%s.root\n" {2000..2923}
+
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/Charmonium/crab_data_Run2018B_UL/210212_112327/0000/RJPsi_data_2021Feb12_%s.root\n" {1..999}
+#printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/Charmonium/crab_data_Run2018B_UL/210212_112327/0001/RJPsi_data_2021Feb12_%s.root\n" {1000..1414}
+
+
+printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/Charmonium/crab_data_Run2018C_UL/210212_112403/0000/RJPsi_data_2021Feb12_%s.root\n" {1..999}
+printf "/gpfs/ddn/srm/cms/store/user/garamire/crab_job_2021Feb12/Charmonium/crab_data_Run2018C_UL/210212_112403/0001/RJPsi_data_2021Feb12_%s.root\n" {1000..1015}
--- scripts/mybatch.py
+++ scripts/mybatch.py
@@ -10,6 +10,7 @@ parser.add_argument('--mc_tau', default='',help='txt file with paths of NanoAOD 
 parser.add_argument('--mc_x', default='',help='txt file with paths of NanoAOD mc that needs to delete signal')
 parser.add_argument('--mc_onia', default='',help='txt file with paths of NanoAOD mc that needs to delete signal')
 parser.add_argument('--mc_gen', default='',help='txt file with paths of NanoAOD generic mc')
+parser.add_argument('--nfilesperjob', default=1,type=int,help='txt file with paths of NanoAOD generic mc')
 
 args = parser.parse_args()
 
